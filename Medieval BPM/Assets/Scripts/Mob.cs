a7c42ccf0073d8200d458fdbf6c7adc68bffdc6d
VladK505 <vladkovalenko622@gmail.com>
2021-10-30 23:26:41 +0300

    Add monsters and broken their spawning

---
+++ Medieval BPM/Assets/Scripts/Mob.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Mobs
+{
+    public class Mob : MonoBehaviour
+    {
+        [SerializeField] private Transform body;
+        [SerializeField] private Transform ragdollBody;
+        [SerializeField] private Animator animator;
+        public int PoolID { get; set; }
+        public NavMeshAgent navMeshAgent;
+
+        //Transform[] pointsArr; // Array of points between witch NPCs should walk 
+        float camDistance; // current distance between npc and camera
+        Vector3 lastPos; // NPC position on last frame
+        float speed; // current npc speed
+
+        void Start()
+        {
+            // Fill up array of pointsOfInterest
+            //Transform _pointsOfInterest = GameObject.FindWithTag("PointsOfInterest").transform;
+           // pointsArr = new Transform[_pointsOfInterest.childCount];
+            lastPos = body.position;
+
+            //for (int _i = 0; _i < _pointsOfInterest.childCount; _i++)
+            //{
+            //    pointsArr[_i] = _pointsOfInterest.GetChild(_i);
+            //}
+        }
+
+        /* Resets position of body and ragdoll to 0 relative to npc root */
+        public void ResetPosition()
+        {
+            body.transform.position = gameObject.transform.position;
+            ragdollBody.transform.position = gameObject.transform.position;
+        }
+
+        /* Stops navMeshAgent when fallen */
+        public void OnLoseBallance()
+        {
+            navMeshAgent.enabled = false;
+        }
+
+        /* Enables navMeshAgent when got up */
+        public void OnRegainBallance()
+        {
+            navMeshAgent.enabled = true;
+        }
+
+        /* Selects between Idle and Walk animation when npc moves */
+        private void UpdateAnimation()
+        {
+            animator.SetFloat("Forward", Mathf.Pow(speed, 7));
+        }
+
+        /* Finds next point to follow and sets it as destination for navMeshAgent */
+        private void GoToPlayer()
+        {
+            navMeshAgent.SetDestination(PlayerPosition.position);
+        }
+
+        public Transform PlayerPosition
+        { private get; set; }
+
+        private void Update()
+        {
+            // Update speed every frame
+            speed = (Vector3.Distance(lastPos, body.position) / Time.deltaTime) / 2.0f;
+            lastPos = body.position;
+        }
+
+        void FixedUpdate()
+        {
+            camDistance = (Camera.main.transform.position -
+                           navMeshAgent.transform.position).sqrMagnitude;
+
+            // If npc is invisible for camera, its animator and ragdoll will be disabled.
+            //if (MobSpawner.Instance.IsVisible(collider.bounds))
+            //{
+            //    animator.enabled = true;
+            //    puppetMaster.mode = PuppetMaster.Mode.Active;
+            //    UpdateAnimation();
+            //}
+            //else
+            //{
+            //    animator.enabled = false;
+            //    puppetMaster.mode = PuppetMaster.Mode.Disabled;
+//
+            //    // If npc to far from camera, it will be pooled;
+            //    if (camDistance > MobSpawner.Instance.npcDistance)
+            //    {
+            //        MobSpawner.Instance.Pool(this);
+            //        //Debug.Log("pool " + name);
+            //    }
+            //}
+
+            // Chooses next destination point 
+            if (navMeshAgent.isOnNavMesh)
+            {
+                GoToPlayer();
+            }
+        }
+    }
+}
---
+++ Medieval BPM/Assets/Scripts/MobSpawner.cs
@@ -0,0 +1,137 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mobs
+{
+    public class MobSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] mobVariants;
+        [SerializeField] private Transform[] spawners;
+        [SerializeField] private Transform spawnersParent;
+        [SerializeField] private Transform mobsParent;
+        [SerializeField] private float spawnRadius = 2f;
+        [SerializeField] private float maxMobDistance = 50f;
+        [SerializeField] private Transform playerPosition;
+        
+        private Plane[] planes;
+        private MobsPool[] mobPools;
+        
+        #region Singleton
+
+        public static MobSpawner Instance; 
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        #endregion
+        
+        // Start is called before the first frame update
+        private void Start()
+        {
+            spawners = new Transform[spawnersParent.childCount];
+
+            for (var i = 0; i < spawnersParent.childCount; i++)
+            {
+                spawners[i] = spawnersParent.GetChild(i);
+            }
+            
+            mobPools = new MobsPool[mobVariants.Length];
+
+            for (var i = 0; i < mobVariants.Length; i++)
+            {
+                mobPools[i] = new MobsPool(mobVariants[i], 4, 3);
+                
+                for (var j = 0; j < mobPools[i].PoolSize; j++)
+                {
+                    var obj = Instantiate(mobPools[i].Prefab, mobsParent);
+                    obj.name += j;
+                    obj.GetComponent<Mob>().PoolID = j;
+                    obj.GetComponent<Mob>().PlayerPosition = playerPosition;
+                    
+                    mobPools[i].AddSpawnedMob(obj.GetComponent<Mob>());
+                }        
+            }
+            
+        }
+        
+        private void Spawn(Transform[] _spawners)
+        {
+            if (_spawners.Length <= 0) return;
+            
+            foreach (var pool in mobPools)
+            {
+                var newMob = pool.spawnedMobs[0];
+                var obj = newMob.gameObject;
+
+                var spawnerIndex = UnityEngine.Random.Range(0, _spawners.Length);
+                var randomX = UnityEngine.Random.Range(-spawnRadius, spawnRadius);
+                var randomZ = UnityEngine.Random.Range(-spawnRadius, spawnRadius);
+                var spawnPosition = new Vector3(_spawners[spawnerIndex].position.x + randomX,
+                    _spawners[spawnerIndex].position.y, _spawners[spawnerIndex].position.z + randomZ);
+                obj.transform.position = spawnPosition;
+                obj.transform.eulerAngles = Vector3.zero;  //reset npc rotation
+
+                pool.RemoveSpawnedMob(newMob);
+                pool.AddActiveMob(newMob);
+            }
+        }
+        
+        /* Moves nps to pool */
+        public void Pool(Mob _mob)
+        {
+            _mob.navMeshAgent.ResetPath();
+            _mob.ResetPosition();
+            mobPools[_mob.PoolID].AddSpawnedMob(_mob);
+            mobPools[_mob.PoolID].AddSpawnedMob(_mob);
+        }
+        
+        /* Checks if bounds visible on camera */
+        public bool IsVisible(Bounds _bounds)
+        {
+            return GeometryUtility.TestPlanesAABB(planes, _bounds);
+        }
+        
+        /* Returns array with spawners transforms that now invisible for camera */
+        private Transform[] GetUnseenSpawners()
+        {
+            var result = new List<Transform>();
+
+            foreach (var spawner in spawners)
+            {
+                var distance = (Camera.main.transform.position - spawner.transform.position).sqrMagnitude;
+
+                if (!IsVisible(spawner.GetComponent<Collider>().bounds) && distance < maxMobDistance)
+                {
+                    result.Add(spawner);
+                }
+            }
+
+            return result.ToArray();
+        }
+        
+        private void LateUpdate()
+        {
+            planes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+
+            var activeMobsCount = 0;
+            
+            foreach (var pool in mobPools)
+            {
+                Debug.Log(pool.MaxActiveNum + " maxActiveNum" + pool.activeMobs.Count +"activeMobs");
+                activeMobsCount = pool.MaxActiveNum - pool.activeMobs.Count;
+            }
+            
+            if (activeMobsCount <= 0) return;
+            Transform[] _spawners = GetUnseenSpawners();
+           
+            for (int i = 0; i < activeMobsCount; i++)
+            {
+                Spawn(_spawners);
+            }
+        }
+
+    }
+}
+
